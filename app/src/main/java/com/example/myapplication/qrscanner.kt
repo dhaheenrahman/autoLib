e24a1be3ab5a3d50f66abb0379a862e834dfe85b
dhaheen <dhaheen.h@gmail.com>
2023-05-22 12:31:56 +0530

    qr_sample_implementation

--- app/src/main/java/com/example/myapplication/qrscanner.kt
+++ app/src/main/java/com/example/myapplication/qrscanner.kt
@@ -1,23 +1,29 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import com.example.myapplication.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.zxing.Result
 import com.google.zxing.ResultPoint
 import com.journeyapps.barcodescanner.BarcodeCallback
 import com.journeyapps.barcodescanner.BarcodeResult
 import com.journeyapps.barcodescanner.CompoundBarcodeView
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+
 
 class QrScanner : AppCompatActivity() {
 
     private lateinit var barcodeView: CompoundBarcodeView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +31,9 @@ class QrScanner : AppCompatActivity() {
         barcodeView = findViewById(R.id.barcodeView)
         barcodeView.decodeContinuous(callback)
         barcodeView.setStatusText("Place the QR pasted on the book in the rectangle")
+        val intent = Intent(this ,BookIssue::class.java)
     }
+
 
     override fun onResume() {
         super.onResume()
@@ -71,6 +79,11 @@ class QrScanner : AppCompatActivity() {
             val barcode: Result? = result.result
             if (barcode != null) {
                 Log.d(TAG, "QR code scanned: " + barcode.text)
+                barcodeView.pause()
+                val bookNo = barcode.text
+                val bookNoField : TextInputEditText? = findViewById<TextInputEditText>(R.id.bookNoField)
+                bookNoField?.setText(bookNo)
+                startActivity(intent)
             }
         }
 
